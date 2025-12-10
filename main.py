c5ba3d62ca79abb82aaa8867bf515aa9b620f101
datascienceanywhere <datascienceanywhere@gmail.com>
2025-12-10 10:20:04 +0530

    router

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from router_example import router
 from fastapi import FastAPI
 
 app = FastAPI()
+app.include_router(router=router)
+
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+
+
